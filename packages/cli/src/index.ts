154406aafd547a916b67feaacfe29094c33e8a05
Willem Meints <1550763+wmeints@users.noreply.github.com>
2026-08-05 11:55:04 +0200

    chore: add placeholder commands

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -1,10 +1,12 @@
 #!/usr/bin/env node
 import { program } from "commander";
 
-program
+const foundry = program
   .name("foundry")
-  .description("Automate your project with factory control loops")
-  .command("init")
-  .action((_args) => {
-    console.log("Hello world");
-  });
+  .description("Automate your project with factory control loops");
+
+
+foundry.command('init').description('Initialize foundry in your project').action(() => console.log('initialize project'));
+foundry.command('run').description('Run a control loop in your project').action(() => console.log('run control loop'));
+
+foundry.parse();
