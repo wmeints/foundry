7e7591cba0153a1697280848cae17d028f41f56c
Willem Meints <1550763+wmeints@users.noreply.github.com>
2026-08-07 06:08:45 +0200

    chore: remove effect

--- vendor/effect/.changeset/pre/tidy-cats-smile.md
+++
@@ -1,5 +0,0 @@
----
-"effect": patch
----
-
-Fix protobuf serialization of negative signed integers to use ten-byte two's-complement varints.
--- vendor/effect/packages/tools/api-diff/src/Error.ts
+++
@@ -1,6 +0,0 @@
-import * as Schema from "effect/Schema"
-
-export class ApiDiffError extends Schema.TaggedError<ApiDiffError>()("ApiDiffError", {
-  message: Schema.String,
-  cause: Schema.optional(Schema.Defect())
-}) {}
